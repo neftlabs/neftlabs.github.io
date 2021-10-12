0dff8781ce9771f9187d87c998e9f287843ad5da
NefT Labs <neftlabs@protonmail.com>
2021-10-12 14:58:01 +0100

    new logo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "b3c70cca65bbe9dc7198a7132b2479fa",
-"/": "b3c70cca65bbe9dc7198a7132b2479fa",
+"index.html": "34713a6f28fb486c5d9f38a026446d04",
+"/": "34713a6f28fb486c5d9f38a026446d04",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3241d1d9c15448a4da96df05f3292ffe",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "ffed6899ceb84c60a1efa51c809a57e4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
-"assets/lib/assets/logo.png": "4665b3800ba316cfcf65ed5f838ec9b4",
+"assets/lib/assets/logo.png": "61f9c86fefbc7caf0fd7b2f948f474b0",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "295a0cfc98a8ee10a4450e504f32b6bb",
 "assets/AssetManifest.json": "6f6c1ba2a1fe68e0771d51dbb46a7e01",
@@ -21,7 +21,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "version.json": "76eb19b14f2e24b7c1e85563a58ea57d",
 "manifest.json": "925d6ac685047279fb37e16320ac9be0",
-"main.dart.js": "28a1e1643f6dbe94a97b16c7aafb5403"
+"main.dart.js": "8ffffbdac04f7a6ee208aabca76213d3"
 };
 
 // The application shell files that are downloaded before a service worker can
