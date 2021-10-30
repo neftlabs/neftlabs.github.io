d91187b39b58dcdd89a6b7f4cfacc2938560de82
NefT Labs <neftlabs@protonmail.com>
2021-10-30 21:31:40 +0100

    updationg favicon

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "d0c60bc9a00f5d283c44c1cac61b3fab",
-"index.html": "8b9baf9f65972e51c50e539af9f9f756",
-"/": "8b9baf9f65972e51c50e539af9f9f756",
+"index.html": "ec498d81a4fa6aa9f1a38cd9e072f7a7",
+"/": "ec498d81a4fa6aa9f1a38cd9e072f7a7",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "5178af1d278432bec8fc830d50996d6f",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b37ae0f14cbc958316fac4635383b6e8",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "aa1ec80f1b30a51d64c72f669c1326a7",
